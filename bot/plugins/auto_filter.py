ef6e8086fb16ba5b4e2847f22d6c678b59ac1057
arjunsangu <86915159+arjunsangu@users.noreply.github.com>
2021-07-06 15:26:02 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -62,7 +62,7 @@ async def auto_filter(bot, update):
     if filters:
         #results.append(
         #        [
-        #            InlineKeyboardButton("⭕️ JOIN OUR MAIN CHANNEL ⭕️", url="https://t.me/UFStudio2")
+        #            InlineKeyboardButton("⭕️ ALL MOVIES CHANEL ⭕️", url="https://t.me/mlm_movie_2")
         #        ]
         #    ) 
         for filter in filters: # iterating through each files
@@ -86,7 +86,7 @@ async def auto_filter(bot, update):
                 file_GB = f"[{str(round((file_size/(1024*1024)),2))} GB]"
                 size = file_GB
                 
-            file_name = size + " - 🎬 " + file_name
+            file_name = size + " - ♻️ " +📥 DOWNLOAD NOW 📥 
             
             print(file_name)
             #file_size = str(file_size) + " KB" if file_size < 1024 elif file_size < 1024 else str(round(file_size/1024)) + " GiB"  #"📁 " + 
@@ -137,8 +137,7 @@ async def auto_filter(bot, update):
     else:
         Send_message=await bot.send_video(
                 chat_id = update.chat.id,
-                video="https://telegra.ph/file/3e9f7db0c98e6b236c2c7.mp4",
-                caption=f"Couldn't Find This Movie.Please Try Again Or Search On Our <b><a href='https://t.me/UFStudio2'>Channel</a></b>. \n\nഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺",
+                caption=f"Sorry Couldn't Find This Movie.Please Try Again Or Search On Our <b><a href='https://t.me/mlm_movie_2'>Channel</a></b>. \n\nഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺",
                 parse_mode="html",
                 reply_to_message_id=update.message_id
             )
@@ -172,7 +171,7 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"⭕🎭 Page 1/{len_result if len_result < max_pages else max_pages} 🎭⭕", callback_data="ignore")
         ])
         
         
@@ -219,7 +218,7 @@ async def auto_filter(bot, update):
         ibuttonss = []
         ibuttonss.append(
                         [
-                            InlineKeyboardButton("⭕️ CONTACT ME ⭕️", url="https://t.me/UFSChatBot")
+                            InlineKeyboardButton("⭕️ ALL MOVIES CHANEL ⭕️", url="https://t.me/mlm_movie_2")
                         ]
                     )
         for x in ibuttonss:
