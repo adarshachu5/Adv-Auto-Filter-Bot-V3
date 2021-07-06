11823a1961780283142372ead225d293f6049ddb
arjunsangu <86915159+arjunsangu@users.noreply.github.com>
2021-07-06 14:46:14 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,60 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) @lnc3f3r Jins Mathew Re-Create
-#Just Add Me To Your Group And Channel And Connect Them And See My Powers 🔥🔥😝
+#😝
 
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>വെറുതെ കുത്തിയിരുന്ന് നോക്കണ്ട എന്നെ എൻ്റെ ഗ്രൂപ്പിൽ മാത്രേ ഉപയോഗിക്കാൻ പറ്റൂ...😁
 
-Only For <b><u><a href="https://t.me/joinchat/TRlZZilyh-MVa66t">Universal Film Studio Group</a></u></b>
+Only For <b><u><a href="https://t.me/MLM_MOVIE_STORE">MLM MOVIE STORE</a></u></b>
 
 For More Details Click Help Button Below..
 
-<b><u><a href="https://t.me/joinchat/YS-WlsUC9nFiOWM0">UFS Bot Support Group</a></u></b>
-</i>"""    
-    
-    HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-<b><u><a href="https://t.me/joinchat/YS-WlsUC9nFiOWM0">UFS Bot Support Group</a></u></b>
-"""
-    
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Advanced Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/lnc3f3r">LuCifer</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://t.me/joinchat/YS-WlsUC9nFiOWM0">Contact Us</a></i>
-"""
+<b><u><a href="https://t.me/mlm_movie_2">ALL MOVIES CHANEL</a></u></b>
+</i>"""
